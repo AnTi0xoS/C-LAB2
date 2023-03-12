2697c89b8155b112dd55986a0dd61caf02f98dbf
Kirill Maksimenko <57628262+AnTi0xoS@users.noreply.github.com>
2023-03-12 20:37:03 +0300

    update files 3

--- Task_1.cpp
+++ Task_1.cpp
@@ -4,12 +4,12 @@
 int main()
 {
 	int n, num = 0;
-    std::cin >> n;
-    std::set <int> numsSet;
-    for (int i = 0; i < n; i++) {
-        std::cin >> num;
-        numsSet.insert(num);
-    }
-    std::cout << numsSet.size();
-    return 0;
+    	std::cin >> n;
+    	std::set <int> numsSet;
+    	for (int i = 0; i < n; i++) {
+        	std::cin >> num;
+        	numsSet.insert(num);
+   	 }
+    	std::cout << numsSet.size();
+    	return 0;
 }
